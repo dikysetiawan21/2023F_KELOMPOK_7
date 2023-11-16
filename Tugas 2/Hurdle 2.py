b1feccb8402f7fd274d7b887c2f953f8a3ca2e0a
23091397178_Achmad Diky Setiawan <149121551+dikysetiawan21@users.noreply.github.com>
2023-11-16 11:33:01 +0700

    Update Hurdle 2.py

--- Tugas 2/Hurdle 2.py
+++ Tugas 2/Hurdle 2.py
@@ -1,1 +1,17 @@
+def belok_kanan():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def lompat():
+    move()
+    turn_left()
+    move()
+    belok_kanan()
+    move()
+    belok_kanan()
+    move()
+    turn_left()
 
+while not at_goal():
+    lompat()
