533fd5a56bba7683d3346afaf945904502bffe02
Tiaradwianinda <149680908+Tiaradwianinda@users.noreply.github.com>
2023-11-16 11:45:25 +0700

    Hurdle 3.py

--- Tugas 2/Hurdle 3.py
+++ Tugas 2/Hurdle 3.py
@@ -1,1 +1,21 @@
-
+def lompat():
+    while not wall_in_front():
+        move()
+        if at_goal():
+            done()
+    turn_left()
+    while wall_on_right():
+        move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    while not wall_in_front():
+        move()
+    turn_left()
+ 
+while not at_goal():
+    lompat()
