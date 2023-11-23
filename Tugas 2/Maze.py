93664c551fadc2edd74c228ab8884fec2efa11a6
achmadharris <149675877+achmadharris@users.noreply.github.com>
2023-11-23 21:45:31 +0700

    Update Maze.py

--- Tugas 2/Maze.py
+++ Tugas 2/Maze.py
@@ -1,1 +1,17 @@
-
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def maze():
+    if not wall_in_front():
+        move()
+        if at_goal():
+            done
+        elif not wall_on_right():
+            turn_right()
+    else:
+        turn_left()    
+       
+while not at_goal():
+    maze()
